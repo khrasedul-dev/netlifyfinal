62183f62353110989313f6f4f66bd4a6fc255a93
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-11-07 15:43:15 +0600

    update

---
+++ functions/bot/bot.js
@@ -0,0 +1,22 @@
+const { Telegraf } = require("telegraf")
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start(ctx => {
+  console.log("Received /start command")
+  try {
+    return ctx.reply("Hi")
+  } catch (e) {
+    console.error("error in start action:", e)
+    return ctx.reply("Error occured")
+  }
+})
+
+exports.handler = async event => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body))
+    return { statusCode: 200, body: "" }
+  } catch (e) {
+    console.error("error in handler:", e)
+    return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+  }
+}
